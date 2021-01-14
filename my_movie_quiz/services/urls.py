5e6f547baa585be76b8f96e8e53fc230c992d275
Totobriac <Totobriac.gmail.com>
2021-01-14 11:42:12 +0100

    have to fix proxy

--- my_movie_quiz/services/urls.py
+++ my_movie_quiz/services/urls.py
@@ -4,5 +4,5 @@ from services import views
 urlpatterns = [    
     path('search_movie/<str:movie>', views.MovieSearch.as_view(), name='movie_search'),
     path('search_actors/<str:actors>', views.ActorsSearch.as_view(), name='actors_search'),
-    path('search_actors', views.TestSearch.as_view(), name='test_search'),
+    path('search_trailer/<str:info>', views.TrailerSearch.as_view(), name='trailer_search')
 ]
--- my_movie_quiz/services/views.py
+++ my_movie_quiz/services/views.py
@@ -2,38 +2,45 @@ import requests
 from django.http import HttpResponse
 from django.views import View
 from rest_framework.decorators import api_view
-
-
-class TestSearch(View):
-    def get(self, request):
-        print('all set up')
+from django.http import JsonResponse
+import subprocess
 
 
 class MovieSearch(View):
+
     def get(self, request, movie):
-
-        url = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + movie
-
+        search_url = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + movie
         headers = {
             'x-rapidapi-key': "3c90722e1fmshd3624c65becfb47p1d49a9jsn722fc9909dc4",
             'x-rapidapi-host': "imdb-internet-movie-database-unofficial.p.rapidapi.com"
             }
-
-        response = requests.request("GET", url, headers=headers)
-
+        response = requests.get( search_url, headers=headers)
         return HttpResponse(response)
+
         
 class ActorsSearch(View):
+
     def get(self, request, actors):
-        print(actors)
+        actorPicUrl= []
+        actors_list = actors.split('-')              
+        subscription_key = "9dc015a3a15c45abb05f88bcea641c2d"
+        search_url = "https://api.bing.microsoft.com/v7.0/images/search"
+        headers = {"Ocp-Apim-Subscription-Key" : subscription_key}
+        for actor in actors_list:
+            response = requests.get(search_url, headers=headers, params={"q": actor, "count": "10"})
+            search_results = response.json()            
+            url_list = [val["thumbnailUrl"] for val in search_results["value"]]
+            actorPicUrl.append(url_list)
+        return JsonResponse(actorPicUrl, safe=False)
 
-        # url = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + movie
 
-        # headers = {
-        #     'x-rapidapi-key': "3c90722e1fmshd3624c65becfb47p1d49a9jsn722fc9909dc4",
-        #     'x-rapidapi-host': "imdb-internet-movie-database-unofficial.p.rapidapi.com"
-        #     }
+class TrailerSearch(View):
 
-        # response = requests.request("GET", url, headers=headers)
-
-        # return HttpResponse(response)
+    def get(self, request, info):
+        search_url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet"
+        params = {"maxResults" : 1, "q": "trailer" + info, "key": "AIzaSyDdU8fgEmwuJP-eL3v_HxNv2AogCmC7wYA"}
+        response = requests.get(search_url, params=params)
+        search_result = response.json()
+        video_id = search_result["items"][0]["id"]["videoId"]
+        video_src = subprocess.run(["youtube-dl", "-g", "https://www.youtube.com/watch?v=" + video_id], encoding='utf-8', stdout=subprocess.PIPE)
+        return JsonResponse(video_src.stdout.split("\n"), safe=False)
